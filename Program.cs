d0ac5f3cc539531cd74116fdc19ec55c043865f6
alireza-moradbakhti <42779520+alireza-moradbakhti@users.noreply.github.com>
2019-06-01 01:07:20 +0430

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Threading.Thread;
+
+
+namespace hbd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Everybody sing along!");
+            Sleep(2000);
+            Beep(264, 125);
+            Sleep(250);
+            Beep(264, 125);
+            Sleep(125);
+            Beep(297, 500);
+            Sleep(125);
+            Beep(264, 500);
+            Sleep(125);
+            Beep(352, 500);
+            Sleep(125);
+            Beep(330, 1000);
+            Sleep(250);
+            Beep(264, 125);
+            Sleep(250);
+            Beep(264, 125);
+            Sleep(125);
+            Beep(297, 500);
+            Sleep(125);
+            Beep(264, 500);
+            Sleep(125);
+            Beep(396, 500);
+            Sleep(125);
+            Beep(352, 1000);
+            Sleep(250);
+            Beep(264, 125);
+            Sleep(250);
+            Beep(264, 125);
+            Sleep(125);
+            Beep(2642, 500);
+            Sleep(125);
+            Beep(440, 500);
+            Sleep(125);
+            Beep(352, 250);
+            Sleep(125);
+            Beep(352, 125);
+            Sleep(125);
+            Beep(330, 500);
+            Sleep(125);
+            Beep(297, 1000);
+            Sleep(250);
+            Beep(466, 125);
+            Sleep(250);
+            Beep(466, 125);
+            Sleep(125);
+            Beep(440, 500);
+            Sleep(125);
+            Beep(352, 500);
+            Sleep(125);
+            Beep(396, 500);
+            Sleep(125);
+            Beep(352, 1000);
+            WriteLine("Yeahhhhh, happy birthday!");
+            ReadLine();
+
+           
+        }
+
+    }
+}
